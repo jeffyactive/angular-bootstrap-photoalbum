db108e993a9a875db40a6746286465d8f20fdee4
jeffyactive <jeff@reelyactive.com>
2017-08-13 12:49:26 -0400

    Added quotes around property names

--- photos/list.js
+++ photos/list.js
@@ -5,21 +5,21 @@
 
 const PHOTOS = [
   {
-    image: "photos/image01.jpg",
-    fullsize: "photos/image01-full.jpg",
-    attributee: "Jeffrey Dungen",
-    license: "cc-by-sa"
+    "image": "photos/image01.jpg",
+    "fullsize": "photos/image01-full.jpg",
+    "attributee": "Jeffrey Dungen",
+    "license": "cc-by-sa"
   },
   {
-    image: "photos/image02.jpg",
-    fullsize: "photos/image02-full.jpg",
-    attributee: "Jeffrey Dungen",
-    license: "cc-by-sa"
+    "image": "photos/image02.jpg",
+    "fullsize": "photos/image02-full.jpg",
+    "attributee": "Jeffrey Dungen",
+    "license": "cc-by-sa"
   },
   {
-    image: "photos/image03.jpg",
-    fullsize: "photos/image03-full.jpg",
-    attributee: "Jeffrey Dungen",
-    license: "cc-by-sa"
+    "image": "photos/image03.jpg",
+    "fullsize": "photos/image03-full.jpg",
+    "attributee": "Jeffrey Dungen",
+    "license": "cc-by-sa"
   }
 ];
