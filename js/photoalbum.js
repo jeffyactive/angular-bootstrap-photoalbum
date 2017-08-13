ff79c4d457642adb850e7210a466f59be5a315d2
jeffyactive <jeff@reelyactive.com>
2017-08-13 10:45:30 -0400

    Implemented URL search

--- js/photoalbum.js
+++ js/photoalbum.js
@@ -5,9 +5,18 @@
 
 angular.module('photoalbum', [ 'ui.bootstrap' ])
 
+  // Config to facilitate photo index search in the URL
+  .config(function($locationProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  })
+
   // Photo controller
-  .controller('PhotoCtrl', function($scope) {
+  .controller('PhotoCtrl', function($scope, $location) {
     prepareSlides(PHOTOS);
+    jumpToSlide();
 
     function prepareSlides(photos) {
       var slides = [];
@@ -23,5 +32,20 @@ angular.module('photoalbum', [ 'ui.bootstrap' ])
       }
       $scope.slides = slides;
     }
+
+    function jumpToSlide() {
+      if($location.search().hasOwnProperty('index')) {
+        var index = parseInt($location.search().index);
+        if((isNaN(index)) || (index < 0) || (index > $scope.slides.length)) {
+          index = 0;
+        }
+        $scope.active = index;
+      }
+    }
+
+    // Update the search in the URL with the active slide's index
+    $scope.$watch('active', function(active) {
+      $location.search( { index: active } );
+    });
 
   });
